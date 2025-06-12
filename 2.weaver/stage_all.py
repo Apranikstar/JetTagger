cc0d815966ada64f4ff886fbed4bb2b71e2434e6
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-06-12 20:14:00 +0330

    Update stage_all.py

--- 2.weaver/stage_all.py
+++ 2.weaver/stage_all.py
@@ -2,10 +2,11 @@ import sys
 import os
 import argparse
 import subprocess
-from subprocess import Popen, PIPE
+import shutil
 from datetime import date
 import time
 import concurrent.futures
+import ROOT
 
 # ________________________________________________________________________________
 def main():
@@ -13,18 +14,25 @@ def main():
 
     parser.add_argument(
         "--indir",
-        help="path input directory",
+        help="Path to input directory",
         default="/eos/experiment/fcc/hh/generation/DelphesEvents/fcc_v07/II/",
     )
     parser.add_argument(
         "--outdir",
-        help="path output directory",
+        help="Path to output directory",
         default=os.path.join(os.getcwd(), "output"),
     )
-
-    parser.add_argument("--sample", help="sample name", default="mgp8_pp_tt_HT_2000_100000_5f_84TeV")
-    parser.add_argument("--ncpus", help="number of cpus", type=int, default=64)
-    parser.add_argument("--opt", help="option 1: run stage 1, 2: run stage 2, 3: all 4: clean", default="3")
+    parser.add_argument(
+        "--sample",
+        help="Sample name template (use {process} as placeholder)",
+        default="mgp8_pp_{process}_HT_2000_100000_5f_84TeV",
+    )
+    parser.add_argument("--ncpus", help="Number of CPUs", type=int, default=64)
+    parser.add_argument(
+        "--opt",
+        help="Option: 1=run stage 1, 2=run stage 2, 3=all, 4=clean",
+        default="3",
+    )
 
     args = parser.parse_args()
     indir = args.indir
@@ -33,128 +41,113 @@ def main():
     sample = args.sample
     opt = args.opt
 
-    ## qq is merge of uu/dd
-    flavors = ["bb", "cc", "ss", "gg", "qq", "tautau","tt"]
+    # Hardcoded flavors list
+    flavors = ["bb", "cc", "ss", "gg", "qq", "tautau", "tt"]
+
+    flavor_to_process = {
+    "qq": "jj",  # THIS is the fix!
+    "bb": "bb",
+    "cc": "cc",
+    "ss": "ss",
+    "gg": "gg",
+    "tautau": "tautau",
+    "tt": "tt",
+}
 
     outtmpdir = os.path.join(os.getcwd(), "tmp")
-    os.system("rm -rf {}".format(outtmpdir))
-    os.system("mkdir -p {}".format(outtmpdir))
-    os.system("mkdir -p {}".format(outdir))
 
-    ## fill name of stage1 files
-    stage1_files = dict()
+    # Clean and create temporary directories
+    if os.path.exists(outtmpdir):
+        shutil.rmtree(outtmpdir)
+    os.makedirs(outtmpdir, exist_ok=True)
+    os.makedirs(outdir, exist_ok=True)
+
+    # Prepare stage1 filenames
+    #stage1_files = {f: f"{outtmpdir}/stage1_H{f}.root" for f in flavors}
+    stage1_files = {
+    f: f"{outtmpdir}/stage1_{flavor_to_process[f]}.root" for f in flavors
+    }
+
     for f in flavors:
-        stage1_files[f] = "{}/stage1_H{}.root".format(outtmpdir, f)
+        #sample_f = sample.format(process=f)
+        sample_f = sample.format(process=flavor_to_process[f])
+        edm_files = f"{indir}/{sample_f}/*.root"
 
-    edm_files = ""
+        # Run stage 1
+        if opt in ["1", "3"]:
+            cmd_stage1 = [
+                "fccanalysis", "run", "stage1.py",
+                "--output", stage1_files[f],
+                "--files-list", edm_files,
+                "--ncpus", str(ncpus)
+            ]
+            print("\nRunning stage 1:\n", " ".join(cmd_stage1), "\n")
+            subprocess.run(" ".join(cmd_stage1), shell=True, check=True)
 
-    for f in flavors:
-        ### run stage 1
-        if opt == "1" or opt == "3":
-
-            sample_f = sample.replace("XX", f)
-            edm_files = "{}/{}/*.root".format(indir, sample_f)
-            cmd_stage1 = (
-                "fccanalysis run stage1.py --output {} --files-list {} --ncpus {}".format(
-                    stage1_files[f], edm_files, ncpus
-                )
-            )
-            print("running stage 1: ")
-            print("")
-            print("{}".format(cmd_stage1))
-            print("")
-            os.system(cmd_stage1)
-
-        ### run stage 2
-        if opt == "2" or opt == "3":
-
+        # Run stage 2
+        if opt in ["2", "3"]:
             nevents = count_events(stage1_files[f])
             nevents_per_thread = int(nevents / ncpus)
 
+            stage2_files = {}
             commands_stage2 = []
-            stage2_files = dict()
-
-            stage2_final_file = "{}/stage2_H{}.root".format(outtmpdir, f)
-            stage2_wild_files = "{}/stage2_H{}_*.root".format(outtmpdir, f)
-            hadd_cmd = "hadd -f {} {}".format(stage2_final_file, stage2_wild_files)
+            stage2_final_file = f"{outtmpdir}/stage2_{f}.root"
+            stage2_wild_files = f"{outtmpdir}/stage2_{f}_*.root"
+            hadd_cmd = f"hadd -f {stage2_final_file} {stage2_wild_files}"
 
             for i in range(ncpus):
-
-                stage2_files[i] = "{}/stage2_H{}_{}.root".format(outtmpdir, f, i)
+                stage2_file = f"{outtmpdir}/stage2_{f}_{i}.root"
+                stage2_files[i] = stage2_file
                 nstart = i * nevents_per_thread
                 nend = nstart + nevents_per_thread
 
-                cmd_stage2 = "python stage2.py {} {} {} {}".format(
-                    stage1_files[f], stage2_files[i], nstart, nend
-                )
-
+                cmd_stage2 = [
+                    "python", "stage2.py",
+                    stage1_files[f],
+                    stage2_file,
+                    str(nstart),
+                    str(nend),
+                ]
                 commands_stage2.append(cmd_stage2)
 
-            # Create a thread pool executor with 4 threads
+            # Run stage 2 in parallel using ProcessPoolExecutor for better CPU usage
+            with concurrent.futures.ProcessPoolExecutor(max_workers=ncpus) as executor:
+                futures = [executor.submit(run_command, cmd) for cmd in commands_stage2]
+                concurrent.futures.wait(futures)
 
-            executor = concurrent.futures.ThreadPoolExecutor(max_workers=ncpus)
+            # Merge stage 2 files
+            print(f"\nMerging stage 2 files into: {stage2_final_file}\n{hadd_cmd}\n")
+            subprocess.run(hadd_cmd, shell=True, check=True)
 
-            # Submit each command to the executor
-            future_to_command = {executor.submit(run_command, command): command for command in commands_stage2}
+            # Copy to output directory
+            shutil.copy(stage2_final_file, outdir)
+            print(f"File copied to: {outdir}")
 
-            # Wait for all threads to finish
-            concurrent.futures.wait(future_to_command)
+            # Cleanup
+            print("Cleaning up temporary files...")
+            for file_path in [stage2_final_file, stage1_files[f]]:
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+            subprocess.run(f"rm -f {stage2_wild_files}", shell=True)
+            print("Done.")
 
-            # Run the final command
-            print("")
-            print("now collect and hadd stage 2 files into: {}".format(stage2_final_file))
-            print(hadd_cmd)
-            os.system(hadd_cmd)
-            print("")
-            print("copy file to final destination...")
-            os.system("cp {} {}".format(stage2_final_file, outdir))
-            print("file {} copied".format(outdir))
-            print("cleaning up tmp files...".format(outdir))
-            os.system("rm -rf {} {} {}".format(stage2_final_file, stage1_files[f], stage2_wild_files))
-            print("done.")
-
+    # Option 4: clean
+    if opt == "4":
+        print("Cleaning temporary directory...")
+        shutil.rmtree(outtmpdir, ignore_errors=True)
+        print("Done.")
 
 # ________________________________________________________________________________
-
-
 def run_command(command):
-    # Replace this with code to run the command
-    print("running command: {}".format(command))
-    os.system(command)
-
+    print(f"Running command: {' '.join(command)}")
+    subprocess.run(command, check=True)
 
 # ________________________________________________________________________________
 def count_events(file, tree_name="events"):
-    import ROOT
-
-    # Open the ROOT file
     root_file = ROOT.TFile.Open(file)
-
-    # Get the tree from the file
     tree = root_file.Get(tree_name)
-
-    # Get the number of events in the tree
-    num_events = tree.GetEntries()
-
-    return num_events
-
+    return tree.GetEntries()
 
 # ________________________________________________________________________________
-def count_events(file, tree_name="events"):
-    import ROOT
-
-    # Open the ROOT file
-    root_file = ROOT.TFile.Open(file)
-
-    # Get the tree from the file
-    tree = root_file.Get(tree_name)
-
-    # Get the number of events in the tree
-    num_events = tree.GetEntries()
-
-    return num_events
-
-
-# _______________________________________________________________________________________
 if __name__ == "__main__":
     main()
