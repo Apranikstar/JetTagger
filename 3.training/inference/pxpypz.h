f88543a052e6f8a1fa0bf6e03ad9626dea67a825
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-08-18 00:10:36 +0330

    Create pxpypz.h

---
+++ 3.training/inference/pxpypz.h
@@ -0,0 +1,64 @@
+#ifndef FCCANALYSES_PPP_H
+#define FCCANALYSES_PPP_H
+
+#include "FCCAnalyses/JetConstituentsUtils.h"
+#include "FCCAnalyses/ReconstructedParticle.h"
+#include "FCCAnalyses/ReconstructedParticle2Track.h"
+#include "FCCAnalyses/ReconstructedParticle2MC.h"
+#include "edm4hep/MCParticleData.h"
+#include "edm4hep/Track.h"
+#include "edm4hep/TrackData.h"
+#include "edm4hep/Cluster.h"
+#include "edm4hep/ClusterData.h"
+#include "edm4hep/CalorimeterHitData.h"
+#include "edm4hep/ReconstructedParticleData.h"
+#include "edm4hep/EDM4hepVersion.h"
+#include "FCCAnalyses/JetClusteringUtils.h"
+#include "fastjet/JetDefinition.hh"
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/Selector.hh"
+
+#include "ROOT/RVec.hxx"
+
+namespace FCCAnalyses {
+namespace JetConstituentsUtils {
+
+class PPP {
+public:
+
+    // 1️⃣ cast_constituent template must be defined before get_px/get_py/get_pz
+    template <typename F>
+    static rv::RVec<FCCAnalysesJetConstituentsData> cast_constituent(
+        const rv::RVec<FCCAnalysesJetConstituents>& jcs, F&& meth)
+    {
+        rv::RVec<FCCAnalysesJetConstituentsData> out;
+        for (const auto &jc : jcs)
+            out.push_back(meth(jc)); // use push_back, simpler
+        return out;
+    }
+
+    // 2️⃣ Cartesian momentum components as static functions
+    static rv::RVec<FCCAnalysesJetConstituentsData> get_px(
+        const rv::RVec<FCCAnalysesJetConstituents>& jcs)
+    {
+        return cast_constituent(jcs, ReconstructedParticle::get_px);
+    }
+
+    static rv::RVec<FCCAnalysesJetConstituentsData> get_py(
+        const rv::RVec<FCCAnalysesJetConstituents>& jcs)
+    {
+        return cast_constituent(jcs, ReconstructedParticle::get_py);
+    }
+
+    static rv::RVec<FCCAnalysesJetConstituentsData> get_pz(
+        const rv::RVec<FCCAnalysesJetConstituents>& jcs)
+    {
+        return cast_constituent(jcs, ReconstructedParticle::get_pz);
+    }
+
+};
+
+} // namespace JetConstituentsUtils
+} // namespace FCCAnalyses
+
+#endif // FCCANALYSES_PPP_H
