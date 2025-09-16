c2d6c9d50ba7ed9b179ca542bbd18492ae717706
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-16 07:16:13 +0200

    Update stage_all.py

--- 2.weaver/stage_all.py
+++ 2.weaver/stage_all.py
@@ -58,7 +58,7 @@ def main():
     "gg": "gg",
     "tautau": "tautau",
     "tt": "tt",
-    "uuddssgg" : "qq",
+    "uuddss" : "qq",
 }
     process = get_process_from_sample(sample)
     if process not in flavor_to_process:
